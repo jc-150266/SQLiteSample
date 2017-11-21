cd5f1b8ceea8af36cbc9a4d0a9d53b3efa1e3954
侑樹 古山 <150266@jc-21.jp>
2017-11-21 14:58:01 +0900

    初回

---
+++ SQLiteSample/SQLiteSample/ISQLite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net;
+
+namespace SQLiteSample
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection(); // <-1
+    }
+}
